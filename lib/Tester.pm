d212294671e7c1e1521d4128fe405ebcb874f47f
brian d foy <brian.d.foy@gmail.com>
2002-10-22 23:55:23 +0000

    * added documentation * added strict (to make Module::Info shut up)
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@530 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/Tester.pm
+++ lib/Tester.pm
@@ -1,4 +1,32 @@
 package Object::Iterate::Tester;
+use strict;
+
+=head1 NAME
+
+Object::Iterate::Tester - test module that uses Object::Iterate
+
+=head1 SYNOPSIS
+
+	use Object::Iterate qw( imap );
+	use Object::Iterate::Tester;
+	
+	my $object = Object::Iterate::Tester->new();
+	
+	my @list = imap { $_ } $object;
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+brian d foy, E<lt>bdfoy@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2002, brian d foy, All rights reserved.
+
+You can use this software under the same terms as Perl itself.
+
+=cut
 
 sub new { bless [qw(a b c d e f)], shift }
 sub __more__ { scalar @{ $_[0] } }
