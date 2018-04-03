e4930a8dc3deb1b7e8d93231891a9a7057d801f8
brian d foy <brian.d.foy@gmail.com>
2018-04-03 15:36:58 -0400

    Change to Artistic License

--- lib/Object/Iterate.pm
+++ lib/Object/Iterate.pm
@@ -261,7 +261,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>.
 Copyright © 2002-2018, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+it under the terms of the Artistic License 2.0.
 
 =cut
 
